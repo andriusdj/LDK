563be28d651e6918dfef32f71c897721b92d4659
Andrius Jankevičius <aj@fostral.net>
2024-05-30 21:00:28 +0200

    fix bug in castle view

--- models/chest.py
+++ models/chest.py
@@ -11,7 +11,7 @@ class Chest(models.Model):
     chest_type = fields.Char(string="Chest Type", store=True, required=True)
 
     recorded = fields.Char(string='Recorded Timestamp', store=True)
-    recorded_date = fields.Datetime(string="Recorded", compute='_compute_recorded', store=True)
+    recorded_date = fields.Datetime(string="Recorded", compute='_compute_recorded')
 
     expiring_in = fields.Char(string='Expiring in', store=True)
 
@@ -33,14 +33,15 @@ class Chest(models.Model):
 
     @api.depends('chest_name', 'chest_type')
     def _compute_value(self):
-        chest_value = self.env['ldk.chest.value'].search(domain=[('chest_name', '=', self.chest_name), ('chest_type', '=', self.chest_type)], limit=1)
-        if not chest_value:
-            chest_value = self.env['ldk.chest.value'].create({
-                'chest_name': self.chest_name,
-                'chest_type': self.chest_type,
-                'value': 0
-            })
-        self.value = chest_value.value
+        for record in self:
+            chest_value = self.env['ldk.chest.value'].search(domain=[('chest_name', '=', record.chest_name), ('chest_type', '=', record.chest_type)], limit=1)
+            if not chest_value:
+                chest_value = self.env['ldk.chest.value'].create({
+                    'chest_name': record.chest_name,
+                    'chest_type': record.chest_type,
+                    'value': 0
+                })
+            record.value = chest_value.value
 
     def init(self):
         res = super().init()
