fb8e5a53dd255bef7332d5f0f61853a905800c3c
Andrius Jankevičius <aj@fostral.net>
2024-06-01 23:30:05 +0200

    calculate the date chest was created

--- models/chest.py
+++ models/chest.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import MissingError
-from datetime import datetime
+from datetime import datetime, timedelta
+import re
 
 class Chest(models.Model):
     _name = 'ldk.chest'
@@ -11,13 +12,48 @@ class Chest(models.Model):
     chest_type = fields.Char(string="Chest Type", store=True, required=True)
 
     recorded = fields.Char(string='Recorded Timestamp', store=True)
-    recorded_date = fields.Datetime(string="Recorded", compute='_compute_recorded')
+    recorded_date = fields.Datetime(string="Recorded", compute='_compute_recorded', store=True)
 
     expiring_in = fields.Char(string='Expiring in', store=True)
+
+    created = fields.Datetime(string='Created', compute='_compute_created', store=True)
 
     value = fields.Integer(string="Value", compute='_compute_value')
             
     castle_id = fields.Many2one('ldk.castle', string="Castle", store=True, required=True)
+
+    @api.depends('recorded_date', 'expiring_in')
+    def _compute_created(self):
+        for rec in self:
+            max_duration = timedelta(hours=20)
+            expiring_in_timedelta = self.parse_expiring_in(expiring_in=rec.expiring_in)
+            time_left = max_duration - expiring_in_timedelta
+            rec.created = rec.recorded_date - time_left
+
+    def parse_expiring_in(self, expiring_in):
+        # Define regex patterns for hours, minutes, and seconds
+        hours_pattern = re.compile(r'(\d+)h')
+        minutes_pattern = re.compile(r'(\d+)m')
+        seconds_pattern = re.compile(r'(\d+)s')
+
+        clean_value = expiring_in.replace('[', '').replace(']', '').replace("'", "").replace(",", " ")
+
+        hours = 0
+        minutes = 0
+        seconds = 0
+
+        try:
+            if hours_match := hours_pattern.search(clean_value):
+                hours = int(hours_match.group(1))
+            if minutes_match := minutes_pattern.search(clean_value):
+                minutes = int(minutes_match.group(1))
+            if seconds_match := seconds_pattern.search(clean_value):
+                seconds = int(seconds_match.group(1))
+        except Exception:
+            # If there's an error in parsing, we use default 0 values
+            pass
+
+        return timedelta(hours=hours, minutes=minutes, seconds=seconds)
 
     @api.depends('recorded')
     def _compute_recorded(self):
