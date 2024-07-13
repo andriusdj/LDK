3237040362e39a72d56036d71e1709d3cbfa3ead
Andrius Jankevičius <aj@fostral.net>
2024-07-13 20:50:36 +0300

    add ragnarok and ancient chests counter

--- models/castle.py
+++ models/castle.py
@@ -21,6 +21,12 @@ class ChestValue(models.Model):
     chest_value_week = fields.Integer(string="Chests Value 7 days", compute='_compute_week_chests')
     chest_value_week2 = fields.Integer(string="Chests Value 14 days", compute='_compute_week_chests')
 
+    chest_count_ragnarok7 = fields.Integer(string="Ragnarok Chests last 7 days", compute='_compute_week_chests')
+    chest_count_ragnarok14 = fields.Integer(string="Ragnarok Chests last 14 days", compute='_compute_week_chests')
+    
+    chest_count_ancient7 = fields.Integer(string="Ancients Chests last 7 days", compute='_compute_week_chests')
+    chest_count_ancient14 = fields.Integer(string="Ancients Chests last 14 days", compute='_compute_week_chests')
+    
     def _compute_week_chests(self):
         current_date = datetime.datetime.now()
         seven_days_ago = current_date - datetime.timedelta(days=7)
@@ -28,6 +34,10 @@ class ChestValue(models.Model):
         for castle in self:
             castle.chest_value_week = sum(chest.value for chest in castle.chest_ids if chest.recorded_date >= seven_days_ago)
             castle.chest_value_week2 = sum(chest.value for chest in castle.chest_ids if chest.recorded_date >= fourteen_days_ago)
+            castle.chest_count_ragnarok7 = count(chest.value for chest in castle.chest_ids if chest.recorded_date >= seven_days_ago and chest.type == "Jormungandr Shop")
+            castle.chest_count_ragnarok14 = count(chest.value for chest in castle.chest_ids if chest.recorded_date >= fourteen_days_ago and chest.type == "Jormungandr Shop")
+            castle.chest_count_ancient7 = count(chest.value for chest in castle.chest_ids if chest.recorded_date >= seven_days_ago and chest.type == "Rise of the Ancients event")
+            castle.chest_count_ancient14 = count(chest.value for chest in castle.chest_ids if chest.recorded_date >= fourteen_days_ago and chest.type == "Rise of the Ancients event")
 
     @api.depends('chest_ids')
     def _compute_chests(self):
